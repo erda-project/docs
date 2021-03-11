e38da70c62ac4417e203be5fdef219cb6a14b3a7
sherotree <wytreeshero@163.com>
2021-03-11 14:34:47 +0800

    feat:  search content in the specific version (#2)
    * feat:  search content in the specific version
    
    * WIP: eliminate magic number
    
    * refactor: refactor code
    
    * fix: fixed PROTOCOL_LENGTH
    
    Co-authored-by: 汪杨 <wy283822@alibaba-inc.com>

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -31,5 +31,39 @@ export default ({
         next();
       }
     })
+
+    siteData.themeConfig.algolia.transformData = suggestions => {
+      const hash ={};
+      const [, version, type] = router.history.current.path.split('/');
+      // 一、在[使用手册]页面搜索时：[使用手册] 当前页面 url 路径包含版本号。若内容为[使用手册]，则在本版本搜索内容；若内容为 [CHANGELOG/FAQ] 则不过滤内容
+      if(type === 'manual') {
+        return suggestions.filter(({ url }) => url.includes('manual') && url.includes(version) || !url.includes('manual'));
+      } 
+      // 二、在[CHANGELOG/FAQ]页面搜索时：[CHANGELOG/FAQ]当前页面 url 路径不包含版本号
+      // 1、搜索内容为[使用手册]时，筛选出有结果的最新版本
+      const manualSuggestions = suggestions.filter(({ url }) => url.includes('manual') && vers.filter((ver) => url.includes(ver)));
+      // 进行版本号的比较，将最新内容放置数组前列
+      manualSuggestions.sort((a, b) => {
+        // url 格式：
+        // https://dice-docs.app.terminus.io/changeLog.html#_3-19
+        // https://doc.app.terminus.io/3.21/manual/support/diagnostics.html#%E6%8F%90%E9%97%AE%E4%B9%8B%E5%89%8D
+
+        const PROTOCOL_LENGTH = 8 // https:// 的长度
+        const version1 = a.url.slice(PROTOCOL_LENGTH).split('/')[1].split('.');
+        const version2 = b.url.slice(PROTOCOL_LENGTH).split('/')[1].split('.');
+        const [versionInteger1, versionDecimal1] = version1;
+        const [versionInteger2, versionDecimal2] = version2;
+        return versionInteger2 === versionInteger1
+          ? versionDecimal2 - versionDecimal1
+          : versionInteger2 - versionInteger1;
+      });
+       // 根据 anchor 字段对相同内容进行过滤显示
+      const latestManualSuggestions = manualSuggestions.reduce(function(arr, current) {
+        return hash[current.anchor] ? arr : arr.concat(current);
+      }, [])
+      // 2、若内容为 [CHANGELOG/FAQ] 则不过滤内容
+      const unManualSuggestions = suggestions.filter(({ url }) => !url.includes('manual'))
+      return unManualSuggestions.concat(latestManualSuggestions)
+    };
   }
 }
